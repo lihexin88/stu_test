931661c333264ccc9f4a7f221b20cd0b768cf603
李贺鑫 <228795709@qq.com>
2018-12-07 08:48:23 +0800

    Update conn.php

--- 系统/conn.php
+++ 系统/conn.php
@@ -1,8 +1,8 @@
 ﻿<?php
 	header("Content-Type: text/html; charset=utf-8");
 	$servername = "localhost";
-	$username = "admin";
-	$password = "kuaile12344";
+	$username = "**********";
+	$password = "**********";
 	$dbname = "stu_test";
 	// 创建连接时直接打开数据库
 	$conn = new mysqli($servername, $username, $password, $dbname);
